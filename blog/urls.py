fed15c3a757128fb684cb29e4c4d6eb0c3e79927
benkivuva <benkivuvak5@gmail.com>
2023-10-24 15:35:47 +0300

    start afresh

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-app_name = 'blog'
-
-urlpatterns = [
-    # Define a URL pattern for the root of the 'blog' app, which renders the 'index.html' template.
-    path('', TemplateView.as_view(template_name="blog/index.html"), name='index'),
-]
