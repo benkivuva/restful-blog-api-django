1b4c9c6078ea97305d12e2cd293de7a743230fab
benkivuva <benkivuvak5@gmail.com>
2023-10-24 16:51:29 +0300

    fixed exposed security key

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%$-%6&j4i7525lk6!#znj@dz4&&zyz)vt)k%4i9*3k2m_8%th6'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
