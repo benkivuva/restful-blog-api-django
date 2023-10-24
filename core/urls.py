1bdcfa1ac0dd0c52a0b0399c72389ee6401bb4eb
benkivuva <benkivuvak5@gmail.com>
2023-10-24 16:24:18 +0300

    fixed errors

---
+++ core/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+
+# Define the URL patterns for the Django project
+urlpatterns = [
+    # Admin URLs: This directs to the Django admin interface
+    path('admin/', admin.site.urls),
+
+    # Include blog app URLs with a namespace called 'blog'
+    path('', include('blog.urls', namespace='blog')),
+
+    # Include blog API app URLs with a namespace called 'blog_api'
+    path('api/', include('blog_api.urls', namespace='blog_api')),
+]
