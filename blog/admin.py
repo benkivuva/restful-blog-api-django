5433766a3bb266675d4e04727be9747990808a4a
benkivuva <benkivuvak5@gmail.com>
2023-10-26 06:21:33 +0300

    add tests

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Post)
+class AuthorAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Post model.
+
+    This configuration defines how the Post model is displayed and managed
+    in the Django admin interface.
+
+    Attributes:
+        list_display (tuple): A tuple of fields to display in the list view of posts.
+        prepopulated_fields (dict): A dictionary specifying fields whose values are
+            automatically generated based on the 'title' field.
+    """
+    list_display = ('title', 'id', 'status', 'slug', 'author')
+    prepopulated_fields = {'slug': ('title',), }
+
+admin.site.register(models.Category)
